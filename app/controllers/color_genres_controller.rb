35cb2f3dc17060f5d6609647daa680091fed0eea
Hanaho Murata <nyaaaaaan0129@gmail.com>
2020-09-05 23:42:43 +0900

    genre_index

--- app/controllers/color_genres_controller.rb
+++ app/controllers/color_genres_controller.rb
@@ -4,6 +4,12 @@ class ColorGenresController < ApplicationController
 		@color_genres = ColorGenre.all
 	end
 
+	def create
+		@color_genre = ColorGenre.new(color_genre_params)
+		@color_genre.save
+		redirect_to new_color_genre_path
+	end
+
     private
 
   def color_genre_params
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -7,6 +7,7 @@ class GenresController < ApplicationController
 	def create
 		@genre = Genre.new(genre_params)
 		@genre.save
+		redirect_to new_genre_path
 	end
 
   private
